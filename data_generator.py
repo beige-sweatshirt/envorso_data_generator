c0edc6b017e5628116d78fbd2b6fec5fbd5a6d7a
BeigeSweatshirt <mholford96@gmail.com>
2023-05-14 03:24:34 -0700

    fixed description generator

--- data_generator.py
+++ data_generator.py
@@ -1,7 +1,7 @@
 import random
 import string
-import loremipsum as lorem
 from datetime import date, timedelta
+from github import Github
 
 def generate_title():
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
@@ -9,9 +9,13 @@ def generate_title():
     return random_string
 
 def generate_description():
-    return lorem.paragraph()
+    with open("lorem.txt", 'r') as file:
+        text = file.read()
+        sentences = text.split(". ")
+        num_sentences = random.randint(1, 3)
+        return ". ".join(random.sample(sentences, num_sentences))
 
-def get_weighted_result(tuple_list):
+def weighted_str(tuple_list):
     dict_list = dict(tuple_list)
     total_prob = sum(dict_list.values())
     rand_num = random.uniform(0, total_prob)
@@ -32,23 +36,32 @@ def generate_duedate(story_pts):
     new_date = today + delta
     return new_date.strftime("%Y-%m-%d")
 
+def open_github_issue(token, user, repo):
+    title = generate_title()
+    descr = generate_description()
+    gh_issue_types = ("bug", 0.75), ("enhancement", 0.25)
+    type = weighted_str(gh_issue_types)
+
+    g = Github(token)
+    repo = g.get_user(user).get_repo(repo)
+    issue = repo.create_issue(title=title, body=descr, labels=[type])
+    print(f"Created issue: {issue.html_url}")
+
 ### Services ###
 # GitHub
-token = "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"
-user = "beige-sweatshirt";
-am_repo = "AM_part";
-oem_repo = "OEM_part"
+gh_token = "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"
+gh_user = "beige-sweatshirt"
+gh_am_repo = "AM_part"
+gh_oem_repo = "OEM_part"
 
 # Jira
-token = "ATATT3xFfGF0-FntKXJfWyO3V-lE1bdSIPPZomhHdAIs72IBofpait0KezLbLLo6wl7aJ4uyDE6hutL3AG6smIP_-Het9a_dDglUrzf6mmRXdAHI6YOSS7YO_89wLZHGURov-74-FvahTxx_BKOAIqCCnp_i5QQI7blJh_PRgHht9kg4qpsOY7w=47CF0519"
-user = "mholford@protonmail.com"
-project_key = "PART"
-am_domain = "am-company"
-oem_domain = "mholford"
+jira_token = "ATATT3xFfGF0-FntKXJfWyO3V-lE1bdSIPPZomhHdAIs72IBofpait0KezLbLLo6wl7aJ4uyDE6hutL3AG6smIP_-Het9a_dDglUrzf6mmRXdAHI6YOSS7YO_89wLZHGURov-74-FvahTxx_BKOAIqCCnp_i5QQI7blJh_PRgHht9kg4qpsOY7w=47CF0519"
+jira_user = "mholford@protonmail.com"
+jira_project_key = "PART"
+jira_am_domain = "am-company"
+jira_oem_domain = "mholford"
 
 ### Data ###
-# GitHub
-gh_issues = ("bug", 0.75), ("enhancement", 0.25)
 
 # Jira
 jira_issues = ("Epic", 0.1), ("Bug", 0.4), ("Story", 0.1), ("Task", 0.4)
@@ -57,3 +70,5 @@ risks = ("Critical", 0.1), ("High", 0.2), ("Medium", 0.4), ("Low", 0.2)
 impacts = ("Extensive", 0.1), ("Significant", 0.2), ("Moderate", 0.4), ("Minor", 0.2)
 sprint_length = 4 # Normally this would be longer, but since we're limited in time.
 
+#print(lorem.get_sentences(2))
+open_github_issue(gh_token, gh_user, gh_oem_repo)
